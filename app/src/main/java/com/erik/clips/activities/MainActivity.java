740389ce96fea66f2f7f1920e31eebcb46414f59
Evgeniy <badula4@gmail.com>
2017-02-07 18:05:47 +0300

    Добавил два активити. Но пока приложение падает. не могу понять в чем проблема

--- app/src/main/java/com/erik/clips/activities/MainActivity.java
+++ app/src/main/java/com/erik/clips/activities/MainActivity.java
@@ -2,36 +2,25 @@ package com.erik.clips.activities;
 
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
-import android.app.AlertDialog;
-import android.app.Fragment;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.TypedArray;
-import android.database.SQLException;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.Toast;
 import com.erik.clips.R;
-import java.io.IOException;
 
 
 @SuppressLint("NewApi")
 public class MainActivity extends FragmentActivity {
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
 
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     // nav drawer title
     private CharSequence mDrawerTitle;
@@ -39,21 +28,12 @@ public class MainActivity extends FragmentActivity {
     // used to store app title
     private CharSequence mTitle;
 
-
-
-    // declare dbhelper and adapter object
-    static DBHelper dbhelper;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTitle = mDrawerTitle = getTitle();
-
-
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         // enabling action bar app icon and behaving it as toggle button
         getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -65,87 +45,64 @@ public class MainActivity extends FragmentActivity {
         // get screen device width and height
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-
-        // checking internet connection
-        if (!Constant.isNetworkAvailable(MainActivity.this)) {
-            Toast.makeText(MainActivity.this, getString(R.string.no_internet), Toast.LENGTH_SHORT).show();
-        }
-
-
-        dbhelper = new DBHelper(this);
-
-        // create database
-        try {
-            dbhelper.createDataBase();
-        } catch (IOException ioe) {
-            throw new Error("Unable to create database");
-        }
-
-        // then, the database will be open to use
-        try {
-            dbhelper.openDataBase();
-        } catch (SQLException sqle) {
-            throw sqle;
-        }
-
-        // if user has already ordered food previously then show confirm dialog
-        if (dbhelper.isPreviousDataExist()) showAlertDialog();
-
-
-            public void onDrawerClosed(View view) {
-                getActionBar().setTitle(mTitle);
-                // calling onPrepareOptionsMenu() to show action bar icons
-                invalidateOptionsMenu();
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                getActionBar().setTitle(mDrawerTitle);
-                // calling onPrepareOptionsMenu() to hide action bar icons
-                invalidateOptionsMenu();
-            }
-        }
     }
 
-    // show confirm dialog to ask user to delete previous order or not
-   public void showAlertDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.confirm);
-        builder.setMessage(getString(R.string.db_exist_alert));
-        builder.setCancelable(false);
-        builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+//        // checking internet connection
+//        if (!Constant.isNetworkAvailable(MainActivity.this)) {
+//            Toast.makeText(MainActivity.this, getString(R.string.no_internet), Toast.LENGTH_SHORT).show();
+//        }
 
-            public void onClick(DialogInterface dialog, int which) {
-                // TODO Auto-generated method stub
-                // delete order data when yes button clicked
-                dbhelper.deleteAllData();
-                dbhelper.close();
-
-            }
-        });
-
-        builder.setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
-
-            public void onClick(DialogInterface dialog, int which) {
-                // TODO Auto-generated method stub
-                // close dialog when no button clicked
-                dbhelper.close();
-                dialog.cancel();
-            }
-        });
-        AlertDialog alert = builder.create();
-        alert.show();
-
+    public void onDrawerClosed(View view) {
+        getActionBar().setTitle(mTitle);
+        // calling onPrepareOptionsMenu() to show action bar icons
+        invalidateOptionsMenu();
     }
+
+    public void onDrawerOpened(View drawerView) {
+        getActionBar().setTitle(mDrawerTitle);
+        // calling onPrepareOptionsMenu() to hide action bar icons
+        invalidateOptionsMenu();
+    }
+
+
+
+//    // show confirm dialog to ask user to delete previous order or not
+//    public void showAlertDialog() {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle(R.string.confirm);
+//        builder.setMessage(getString(R.string.db_exist_alert));
+//        builder.setCancelable(false);
+//        builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+//
+//            public void onClick(DialogInterface dialog, int which) {
+//                // TODO Auto-generated method stub
+//                // delete order data when yes button clicked
+//                dbhelper.deleteAllData();
+//                dbhelper.close();
+//
+//            }
+//        });
+//
+//        builder.setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+//
+//            public void onClick(DialogInterface dialog, int which) {
+//                // TODO Auto-generated method stub
+//                // close dialog when no button clicked
+//                dbhelper.close();
+//                dialog.cancel();
+//            }
+//        });
+//        AlertDialog alert = builder.create();
+//        alert.show();
+//
+//    }
 
     @Override
     public void onBackPressed() {
         // TODO Auto-generated method stub
-        dbhelper.deleteAllData();
-        dbhelper.close();
         finish();
         overridePendingTransition(R.anim.open_main, R.anim.close_next);
     }
-
 
 
     @Override
@@ -179,12 +136,11 @@ public class MainActivity extends FragmentActivity {
     }
 
 
-
     @Override
     public void setTitle(CharSequence title) {
         mTitle = title;
         getActionBar().setTitle(mTitle);
     }
-
 }
+
 
